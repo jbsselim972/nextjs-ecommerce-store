3324d26c6401a3d864916dd7365a93eb81c72b7b
jbsselim <jbsselim@gmail.com>
2023-07-06 12:13:35 -0400

    added Modal component

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import PreviewModal from "@/components/PreviewModal";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <PreviewModal />
+    </>
+  );
+};
+
+export default ModalProvider;
