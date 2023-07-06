a2e894bf2619145179fbbd358259e3a96b701fda
jbsselim <jbsselim@gmail.com>
2023-07-06 12:10:37 -0400

    declaring types

---
+++ types.d.ts
@@ -0,0 +1,39 @@
+interface Billboard {
+  id: string;
+  label: string;
+  imageUrl: string;
+}
+
+interface Category {
+  id: string;
+  name: string;
+  billboard: Billboard;
+}
+
+interface Product {
+  id: string;
+  category: Category;
+  name: string;
+  price: string;
+  isFeature: boolean;
+  size: Size;
+  color: Color;
+  images: ImageType[];
+}
+
+interface ImageType {
+  id: string;
+  url: string;
+}
+
+interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
+
+interface Color {
+  id: string;
+  name: string;
+  value: string;
+}
