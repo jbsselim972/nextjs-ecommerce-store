9f450ec181a6e98a78c86dcad6c04cf74659189f
jbsselim <jbsselim@gmail.com>
2024-01-26 18:13:30 -0400

    removed billboard from the categoryId page

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -38,7 +38,7 @@ const CategoryPage: FC<CategoryPageProps> = async ({
   return (
     <div className="bg-white">
       <Container>
-        <Billboard data={category.billboard} />
+        {/* <Billboard data={category.billboard} /> */}
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-col-5 lg:gap-x-8">
             {/**  Mobile Filters  */}
